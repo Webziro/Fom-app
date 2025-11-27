8a8173465a6440d27beba01807feef4160292b42
Stanley Amaziro <stanleyamaziro@gmail.com>
2025-11-27 16:15:34 +0100

    Backend: Group uploads of file

--- finfom-frontend/src/api/groups.js
+++ finfom-frontend/src/api/groups.js
@@ -1,15 +1,3 @@
-// import axios from './axios';
-
-// export const groupsAPI = {
-//   createGroup: (data) => axios.post('api/groups', data),
-//   getMyGroups: () => axios.get('api/groups'),
-//   getGroup: (id) => axios.get(`api/groups/${id}`),
-//   getGroupFiles: (id, params) => axios.get(`api/groups/${id}/files`, { params }),
-//   updateGroup: (id, data) => axios.put(`api/groups/${id}`, data),
-//   deleteGroup: (id) => axios.delete(`api/groups/${id}`),
-// };
-
-
 import axios from './axios';
 
 export const groupsAPI = {
