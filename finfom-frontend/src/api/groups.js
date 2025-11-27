3c61e8e6a5d2cb68576c49e9a5a821867154cf12
Stanley Amaziro <stanleyamaziro@gmail.com>
2025-11-27 15:55:10 +0100

    Backend: Group uploads of file

--- finfom-frontend/src/api/groups.js
+++ finfom-frontend/src/api/groups.js
@@ -1,10 +1,25 @@
+// import axios from './axios';
+
+// export const groupsAPI = {
+//   createGroup: (data) => axios.post('api/groups', data),
+//   getMyGroups: () => axios.get('api/groups'),
+//   getGroup: (id) => axios.get(`api/groups/${id}`),
+//   getGroupFiles: (id, params) => axios.get(`api/groups/${id}/files`, { params }),
+//   updateGroup: (id, data) => axios.put(`api/groups/${id}`, data),
+//   deleteGroup: (id) => axios.delete(`api/groups/${id}`),
+// };
+
+
 import axios from './axios';
 
 export const groupsAPI = {
+  // Existing functions
   createGroup: (data) => axios.post('api/groups', data),
-  getMyGroups: () => axios.get('api/groups'),
+  getMyGroups: () => axios.get('api/groups/my-groups'), // Fixed to use correct endpoint
   getGroup: (id) => axios.get(`api/groups/${id}`),
   getGroupFiles: (id, params) => axios.get(`api/groups/${id}/files`, { params }),
   updateGroup: (id, data) => axios.put(`api/groups/${id}`, data),
   deleteGroup: (id) => axios.delete(`api/groups/${id}`),
+  getAllGroups: () => axios.get('api/groups'), 
+  createOrGetGroup: (data) => axios.post('api/groups/create-or-get', data), 
 };
