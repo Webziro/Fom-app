bd5e7a24c7964e7ea707ecef24a3ca5ac4d6df8b
Stanley Amaziro <stanleyamaziro@gmail.com>
2025-12-22 11:40:22 +0100

    Implimented Redis

--- finfom-backend/src/controllers/fileController.ts
+++ finfom-backend/src/controllers/fileController.ts
@@ -185,6 +185,7 @@ export const uploadFile = async (req: AuthRequest, res: Response) => {
         }
       );
 
+      // Pipe the buffer to Cloudinary upload stream
       const stream = new Readable();
       stream.push(buffer);
       stream.push(null);
